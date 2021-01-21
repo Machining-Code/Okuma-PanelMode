1f30822c2707af8bba37dd93475600dea1df43b9
Phil Coltrane <phil.m.coltrane@gmail.com>
2021-01-20 20:06:18 -0500

    Add minimal logging

--- Okuma.PanelMode/App.xaml.cs
+++ Okuma.PanelMode/App.xaml.cs
@@ -13,5 +13,30 @@ namespace Okuma.PanelMode
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Serilog.Log.Information("Started application {@Name}", System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+            Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+        }
+
+        private void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Serilog.Log.Information("Error in {@Name}: [{Type}] {Message", 
+                System.Reflection.Assembly.GetExecutingAssembly().GetName().Name,
+                e.Exception?.GetType()?.Name,
+                e.Exception?.Message
+                );
+            MessageBox.Show($"An error has occurred: {e.Exception?.Message}");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Serilog.Log.Information("Exited application {@Name}", System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
+
+            base.OnExit(e);
+        }
     }
 }
